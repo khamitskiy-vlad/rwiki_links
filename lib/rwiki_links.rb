8de613a36741b449e00213e7fde6474b3592e174
Vladislav Khamitskiy <vladislaviktorovic@gmail.com>
2025-01-25 21:54:29 +0300

    init gem

---
+++ lib/rwiki_links.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rwiki_links/version"
+
+module RwikiLinks
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/rwiki_links_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RwikiLinks do
+  it "has a version number" do
+    expect(RwikiLinks::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
